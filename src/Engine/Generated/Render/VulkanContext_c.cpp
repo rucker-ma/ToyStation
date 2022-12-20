9036486f6bc61ce652d030949232f533d48aae93
macr <cr.ma@t2cn.com>
2022-12-20 15:02:32 +0800

    update format.yml

--- src/Engine/Generated/Render/VulkanContext_c.cpp
+++ src/Engine/Generated/Render/VulkanContext_c.cpp
@@ -1,21 +1,18 @@
 #include "VulkanContext_c.h"
-void VulkanContext_CreateInstance(TSEngine::DebugFunction func, bool EnableDebug)
-{
-    return TSEngine::VulkanContext::Instance().CreateInstance(func, EnableDebug);
+void VulkanContext_CreateInstance(TSEngine::DebugFunction func,
+                                  bool EnableDebug) {
+    return TSEngine::VulkanContext::Instance().CreateInstance(func,
+                                                              EnableDebug);
 }
-void VulkanContext_CreateSurface(HWND hwnd)
-{
+void VulkanContext_CreateSurface(HWND hwnd) {
     return TSEngine::VulkanContext::Instance().CreateSurface(hwnd);
 }
-TSEngine::VkCtx VulkanContext_GetContext()
-{
+TSEngine::VkCtx VulkanContext_GetContext() {
     return TSEngine::VulkanContext::Instance().GetContext();
 }
-void * VulkanContext_GetDeviceProcAddr(char * name)
-{
+void* VulkanContext_GetDeviceProcAddr(char* name) {
     return TSEngine::VulkanContext::Instance().GetDeviceProcAddr(name);
 }
-void * VulkanContext_GetInstanceProcAddr(char * name)
-{
+void* VulkanContext_GetInstanceProcAddr(char* name) {
     return TSEngine::VulkanContext::Instance().GetInstanceProcAddr(name);
 }
--- src/Engine/Source/Render/RenderUtils.h
+++ src/Engine/Source/Render/RenderUtils.h
@@ -3,22 +3,31 @@
 
 namespace TSEngine {
 
-    class RenderUtils
-    {
-    public:
-        RenderUtils(VkCommandPool Pool);
-        void CopyBufferToImage(VkBuffer Buffer, VkImage Image, uint32_t Width, uint32_t Height);
-        void CopyBufferToGPU(void* Data, VkDeviceSize DataSize, VkBuffer& Buffer, VkDeviceMemory& Memory, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties);
-        void CreateGPUBuffer(VkDeviceSize Size, VkBufferUsageFlags Usage, VkMemoryPropertyFlags Properties, VkBuffer& Buffer, VkDeviceMemory& BufferMemory);
-        void TransitionImageLayout(VkImage Image, VkImageLayout OldLayout, VkImageLayout NewLayout,
-        VkAccessFlags OldAccessMask,VkAccessFlags NewAccessMask);
-        void TransitionImageLayoutWithCommandBuffer(VkCommandBuffer CommandBuffer,VkImage Image, VkImageLayout OldLayout, VkImageLayout NewLayout,
-     VkAccessFlags OldAccessMask,VkAccessFlags NewAccessMask);
-        VkCommandBuffer BeginSingletimeCommand();
-        void EndSingletimeCommand(VkCommandBuffer CommandBuffer);
-    private:
-        
-         VkCommandPool command_pool_;
-    };
-}
+class RenderUtils {
+public:
+    RenderUtils(VkCommandPool Pool);
+    void CopyBufferToImage(VkBuffer Buffer, VkImage Image, uint32_t Width,
+                           uint32_t Height);
+    void CopyBufferToGPU(void* Data, VkDeviceSize DataSize, VkBuffer& Buffer,
+                         VkDeviceMemory& Memory, VkBufferUsageFlags usage,
+                         VkMemoryPropertyFlags properties);
+    void CreateGPUBuffer(VkDeviceSize Size, VkBufferUsageFlags Usage,
+                         VkMemoryPropertyFlags Properties, VkBuffer& Buffer,
+                         VkDeviceMemory& BufferMemory);
+    void TransitionImageLayout(VkImage Image, VkImageLayout OldLayout,
+                               VkImageLayout NewLayout,
+                               VkAccessFlags OldAccessMask,
+                               VkAccessFlags NewAccessMask);
+    void TransitionImageLayoutWithCommandBuffer(VkCommandBuffer CommandBuffer,
+                                                VkImage Image,
+                                                VkImageLayout OldLayout,
+                                                VkImageLayout NewLayout,
+                                                VkAccessFlags OldAccessMask,
+                                                VkAccessFlags NewAccessMask);
+    VkCommandBuffer BeginSingletimeCommand();
+    void EndSingletimeCommand(VkCommandBuffer CommandBuffer);
 
+private:
+    VkCommandPool command_pool_;
+};
+}  // namespace TSEngine
